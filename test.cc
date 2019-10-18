82e43f0bac8382264bc21f164905779c757dd6a8
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2019-10-18 18:36:18 -0400

    remove debug outputs

--- test.cc
+++ test.cc
@@ -18,13 +18,5 @@ struct D : B, C {
 };
 
 int main(int argc, const char *argv[]) {
-	using init_set = boost::mpl::set<>;
-	using setA = boost::mpl::insert<init_set, A>::type;
-	using setAB = boost::mpl::insert<setA, B>::type;
-	
-	using initSetAB = boost::mpl::set<A,B>;
-	std::cout << CxxFFI::detail::readableName<init_set>() << " -> " << CxxFFI::detail::readableName<setA>() << std::endl;
-	std::cout << CxxFFI::detail::readableName<setA>() << " -> " << CxxFFI::detail::readableName<setAB>() << std::endl;
-	std::cout << CxxFFI::detail::readableName<setAB>() << " =? " << CxxFFI::detail::readableName<initSetAB>() << std::endl;
 	std::cout << CxxFFI::CastsTable<A, B, C, D>::apply() << std::endl;
 };
