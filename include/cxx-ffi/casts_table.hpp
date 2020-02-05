a2e8bbb77f7e8b2e21ea4ae9a7952b6efd95620f
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2020-02-05 13:47:00 -0500

    Move legacy python code aside to prevent confusion; added explicit LGPL

--- include/cxx-ffi/casts_table.hpp
+++ include/cxx-ffi/casts_table.hpp
@@ -1,13 +1,23 @@
-//
-//  casts_table.hpp
-//  Cxx-FFI
-//
-//  Created by Thomas Dickerson on 10/18/19.
-//
-//
-
-
 #pragma once
+/************************************************************************************
+ *
+ * Author: Thomas Dickerson
+ * Copyright: 2019 - 2020, Geopipe, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************************/
 
 #include <boost/core/demangle.hpp>
 
